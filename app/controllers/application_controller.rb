a9282c46a66031bfa23209732e2b48185dfea717
maruko92188 <maruko.kazuki.f@gmail.com>
2026-06-23 04:00:06 +0900

    想定外のlocaleが指定されてもエラーにならないように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
 
   def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
+    locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|ja/ do
+  scope "(:locale)" do
     resources :books
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
