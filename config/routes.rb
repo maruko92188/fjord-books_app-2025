55deb23f59cc085c5e7e81484c1ac1a8502cdd48
maruko92188 <maruko.kazuki.f@gmail.com>
2026-08-02 20:26:16 +0900

    アカウント編集後のリダイレクト先を詳細画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "books#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations'}
   resources :books
   resources :users, only: [:index, :show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
