0cbb86542fdd99118b8615ddcd9f307f9323241a
maruko92188 <maruko.kazuki.f@gmail.com>
2026-08-05 23:22:40 +0900

    画像のリサイズ機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
