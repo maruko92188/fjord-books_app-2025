7b111298f88f1a9e52c251bdd578797016468cc1
maruko92188 <maruko.kazuki.f@gmail.com>
2026-08-06 22:19:53 +0900

    rubocopの指摘箇所を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_one_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
   end
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
