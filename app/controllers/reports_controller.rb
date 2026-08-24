dd9ff57c3407cc581a3b029672faab0efdd3d542
maruko92188 <maruko.kazuki.f@gmail.com>
2026-08-24 22:49:40 +0900

    Reportモデルのスタイリングとi18n対応
    タイムゾーンを東京に設定
    ナビゲーションバーに日報一覧へのリンクを追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -18,7 +18,7 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.build(report_params)
     if @report.save
-      redirect_to @report, notice: 'Report was successfully created.'
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,7 +26,7 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      redirect_to @report, notice: 'Report was successfully updated.'
+      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
 
   def destroy
     @report.destroy!
-    redirect_to reports_path, status: :see_other, notice: 'Report was successfully destroyed.'
+    redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
