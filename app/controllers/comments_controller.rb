3ff97589d25c8bca97ecae338a537a70d33cfee1
maruko92188 <maruko.kazuki.f@gmail.com>
2026-08-25 00:09:07 +0900

    コントローラーを作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :set_own_comment, only: %i[edit update destroy]
+
+  def edit; end
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+
+    @comment.save
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+  end
+
+  def update
+    @comment.update(comment_params)
+    redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+  end
+
+  def destroy
+    @comment.destroy!
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  private
+
+  def set_own_comment
+    @comment = current_user.comments.find(params.expect(:id))
+  end
+
+  def report_params
+    params.expect(report: [:body])
+  end
+end
