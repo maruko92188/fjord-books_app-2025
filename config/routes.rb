2d7c401a0db77ef10f776c3bc7bf70141088c474
maruko92188 <maruko.kazuki.f@gmail.com>
2026-08-25 00:09:48 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :reports
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
   resources :users, only: %i(index show)
+  resources :books do
+    resources :comments, only: %i[edit create update destroy]
+  end
+  resources :reports do
+    resources :comments, only: %i[edit create update destroy]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
